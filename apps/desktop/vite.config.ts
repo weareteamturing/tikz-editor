4a2de9eb4f274cfc0ed326f30a11c030d974ccdb
Dominik Peters <3543224+DominikPeters@users.noreply.github.com>
2026-03-14 02:54:45 +0100

    worker must use es

--- apps/desktop/vite.config.ts
+++ apps/desktop/vite.config.ts
@@ -18,6 +18,9 @@ export default defineConfig({
     strictPort: true
   },
   envPrefix: ["VITE_", "TAURI_"],
+  worker: {
+    format: "es"
+  },
   build: {
     target: process.env.TAURI_ENV_PLATFORM === "windows" ? "chrome105" : "safari13",
     minify: !process.env.TAURI_ENV_DEBUG ? "esbuild" : false,
